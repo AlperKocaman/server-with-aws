52c0312148c9b306a96cb7b568c482043ef308b2
Alper Kocaman <alperkman@gmail.com>
2022-08-06 18:20:52 +0300

    Init conf in server setup phase

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/AlperKocaman/server-with-aws/cmd/config"
 	"github.com/AlperKocaman/server-with-aws/core/server"
 	"log"
 )
@@ -13,10 +14,13 @@ func main() {
 		}
 	}()
 
-	// TODO: need to read config
+	err := config.InitializeConfig()
+	if err != nil {
+		log.Fatal("error while reading config, exiting.")
+	}
 
 	log.Println("Starting http server...")
-	err := server.InitializeServer()
+	err = server.InitializeServer()
 	if err != nil {
 		log.Fatal("error while starting server, exiting.")
 		return
