7e52545901d6641fd8af91310cd7aeaf3f3c92d6
Alper Kocaman <alperkman@gmail.com>
2022-08-06 23:11:04 +0300

    Add service unable error(503) for AWS errors return type

--- go/core/response/response.go
+++ go/core/response/response.go
@@ -13,24 +13,57 @@ const (
 	HTTPStatusOKCode      = http.StatusOK
 	BadRequestErrorCode   = http.StatusBadRequest
 	InternalServerErrCode = http.StatusInternalServerError
+	ServiceUnableErrCode  = http.StatusServiceUnavailable
 )
 
 var (
 	BadRequest          = errors.New("bad request")
 	InternalServerError = errors.New("internal server error")
+	ServiceUnableError  = errors.New("service unable")
 )
+
+type Generic struct {
+	// example: 200
+	Code int `json:"code"`
+	// example: true
+	Success bool `json:"success"`
+}
+
+type GenericSuccess struct {
+	Generic
+	Data interface{} `json:"data"`
+}
+
+type GenericError struct {
+	Generic
+	// example: service unable
+	Message string `json:"message"`
+}
 
 func Generate(data Responder, err error) (int, interface{}) {
 	if err != nil {
+		resp := GenericError{}
 		if errors.Is(err, BadRequest) {
-			return BadRequestErrorCode, err.Error()
+			resp.Generic.Code = BadRequestErrorCode
+			resp.Message = err.Error()
+		} else if errors.Is(err, InternalServerError) {
+			resp.Generic.Code = InternalServerErrCode
+			resp.Message = err.Error()
+		} else if errors.Is(err, ServiceUnableError) {
+			resp.Generic.Code = ServiceUnableErrCode
+			resp.Message = err.Error()
 		}
-
-		if errors.Is(err, InternalServerError) {
-			return InternalServerErrCode, err.Error()
-		}
+		return resp.Code, resp
 	}
 
-	return HTTPStatusOKCode, data.Response()
+	resp := GenericSuccess{
+		Generic: Generic{
+			Code:    HTTPStatusOKCode,
+			Success: true,
+		},
+		Data: data.Response(),
+	}
+
+	return HTTPStatusOKCode, resp
 
 }
