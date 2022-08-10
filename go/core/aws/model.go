15866dadca6acddd799bb2101dde2484ab8211c1
Alper Kocaman <alperkman@gmail.com>
2022-08-11 01:08:43 +0300

    Delete last modified time from responses

--- go/core/aws/model.go
+++ go/core/aws/model.go
@@ -1,16 +1,12 @@
 package aws
 
-import "time"
-
 type Object struct {
-	Key          string     `json:"key"`
-	LastModified *time.Time `json:"last_modified"`
-	Size         int64      `json:"size_in_bytes"`
+	Key  string `json:"key"`
+	Size int64  `json:"size_in_bytes"`
 }
 
 type Content struct {
-	Data          string     `json:"data"`
-	LastModified  *time.Time `json:"last_modified"`
-	ContentLength int64      `json:"content_length"`
-	ContentType   string     `json:"content_type"`
+	Data          string `json:"data"`
+	ContentLength int64  `json:"content_length"`
+	ContentType   string `json:"content_type"`
 }
